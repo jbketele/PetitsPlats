c6b0d30e0a2c47fe86dde148a15b430b79dda081
Jb Ketele <149502977+jbketele@users.noreply.github.com>
2025-01-06 15:18:09 +0100

    cards recettes

--- recipes.js
+++ recipes.js
@@ -16,20 +16,21 @@ const recipes = [
             },
             {
                 "ingredient": "Crème de coco",
-                "quantity": 2,
+                "quantity": 4,
                 "unit": "cuillères à soupe"
             },
             {
                 "ingredient": "Sucre",
-                "quantity": 30,
+                "quantity": 20,
                 "unit": "grammes"
             },
             {
-                "ingredient": "Glaçons"
+                "ingredient": "Glaçons",
+                "quantity": 2
             }
         ],
         "time": 10,
-        "description": "Mettre les glaçons à votre goût dans le blender, ajouter le lait, la crème de coco, le jus de 2 citrons et le sucre. Mixer jusqu'à avoir la consistence désirée",
+        "description": "Mettre les glaçons à votre goût dans le blender, ajouter le lait, la crème de coco, le jus de 2 citrons et le sucre. Mixer jusqu'à avoir la consistence désirée.",
         "appliance": "Blender",
         "ustensils": ["cuillère à Soupe", "verres", "presse citron"]
     },
@@ -67,7 +68,7 @@ const recipes = [
             }
         ],
         "time": 60,
-        "description": "Découper le thon en dés, mettre dans un plat et recouvrir de jus de citron vert (mieux vaut prendre un plat large et peu profond). Laisser reposer au réfrigérateur au moins 2 heures. (Si possible faites-le le soir pour le lendemain. Après avoir laissé mariner le poisson, coupez le concombre en fines rondelles sans la peau et les tomates en prenant soin de retirer les pépins. Rayer la carotte. Ajouter les légumes au poissons avec le citron cette fois ci dans un Saladier. Ajouter le lait de coco. Pour ajouter un peu plus de saveur vous pouvez ajouter 1 à 2 cuillères à soupe de Crème de coco",
+        "description": "Découper le thon en dés, mettre dans un plat et recouvrir de jus de citron vert (mieux vaut prendre un plat large et peu profond). Laisser reposer au réfrigérateur au moins 2 heures. \n(Si possible faites-le le soir pour le lendemain. Après avoir laissé mariner le poisson, coupez le concombre en fines rondelles sans la peau et les tomates en prenant soin de retirer les pépins. Rayer la carotte. Ajouter les légumes au poissons avec le citron cette fois ci dans un Saladier. Ajouter le lait de coco. Pour ajouter un peu plus de saveur vous pouvez ajouter 1 à 2 cuillères à soupe de Crème de coco",
         "appliance": "Saladier",
         "ustensils": ["presse citron"]
     }, {
--- script.js
+++ script.js
@@ -6,13 +6,37 @@ recipes.forEach(recipe => {
     const card = document.createElement("div");
     card.classList.add("recipe-card");
 
+    // Construire les ingrédients sous forme de liste avec les quantités en dessous
+    const ingredientGrid = recipe.ingredients
+        .map((item) => {
+            const quantity = item.quantity ? `${item.quantity}` : "";
+            const unit = item.unit ? `${item.unit}` : "";
+            return `
+                <div class="ingredient-item">
+                    <span class="ingredient-name">${item.ingredient}</span>
+                    <span class="ingredient-quantity">${quantity} ${unit}</span>
+                </div>
+            `;
+        })
+        .join("");
+
+
     //Contenu HTML de la carte
     card.innerHTML = `
         <img src="${recipe.image}" alt="${recipe.title}" class="recipe-image">
-        <div class="recipe-content">
+        <p class= "time">${recipe.time}min</p>
+        <div class="recipe-body">
             <h2 class="recipe-name">${recipe.name}</h2>
-            <p class="recipe-description">${recipe.description}</p>
-            <p class="recipe-time">Temps de cuisson : ${recipe.time}</p>
+            <div class="recipe">
+                <div class="recipe-content">
+                    <h3>recette</h3>
+                    <p class="recipe-description">${recipe.description.replace(/\n/g, "<br>")}</p>
+                </div>
+                <div class="recipe-ingredients">
+                    <h3>ingrédients</h3>
+                    <div class= "ingredient-grid">${ingredientGrid}</div>
+                </div>
+            </div>
         </div>
     `;
 
